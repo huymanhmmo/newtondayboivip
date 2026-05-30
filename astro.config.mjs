4a1e236e6df96c053c8339d4a907d2f2a1751055
huymanhmmo <huymanh.mmo@gmail.com>
2026-05-30 23:50:36 +0700

    Configure sitemap generation in astro.config.mjs and install dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://newton.dayboi.vip',
   adapter: cloudflare({
     imageService: 'passthrough'
   }),
   build: {
     assets: 'assets'
-  }
+  },
+  integrations: [sitemap()]
 });
